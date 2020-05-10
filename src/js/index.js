e4bfe556390549430d9cf6a2dee97e770d1c4c07
MonMon201 <danyavasilinenko213@gmail.com>
2020-05-10 17:17:42 +0300

    Edits

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,10 @@
-// const SurveyDB = require('./SurveyDB.class');
 const { getQuestionary } = require('./SurveyDB.module');
-
-// const survey = new SurveyDB();
 
 const questionType = 1;
 const pollName = 'Соц. опитування мешканців гуртожитку';
 // const pollName = 'Соц. опитування киян';
 
 (async () => {
-  // const questionary = await survey.getQuestionary(1);
-  // console.log(questionary);
-  // survey.closeConnection();
   const questionary = await getQuestionary(pollName, questionType);
   console.dir(JSON.stringify(questionary), { depth: null });
 })();
